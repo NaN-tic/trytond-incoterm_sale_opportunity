b04f6a24d3864fe02c2a3273e6f63fb742bfbf37
Guillem Barba <guillem@nan-tic.com>
2015-06-17 18:20:32 +0200

    Initial import from github repo

---
+++ tests/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    __init__
+
+    Test Suite
+
+    :copyright: (c) 2014 by Openlabs Technologies & Consulting (P) Limited
+    :license: BSD, see LICENSE for more details.
+"""
+
+from .test_incoterm_sale_opportunity import suite
+
+__all__ = ['suite']
